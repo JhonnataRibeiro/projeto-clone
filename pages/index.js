5fc4a2337c40d5ce587f75c46e637f62b3171227
jon96ribeiro@gmail.com <jon96ribeiro@gmail.com>
2023-12-28 21:51:05 -0300

    adicionado scripts `lint:check`e `lint:fiz`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Olha, agora tem uma modificação servera</h1>
+  return <h1>Olha, agora tem uma modificação servera</h1>;
+}
+
+function meuOvo() {
+  console.log("oi oi oi");
 }
 
 export default Home;
